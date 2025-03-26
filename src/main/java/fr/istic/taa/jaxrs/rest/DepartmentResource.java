47b7ae7b6b3079c23251bf4473a02352142536a8
Savi Oumar <oumar.savi@etudiant.univ-rennes.fr>
2025-03-26 14:30:08 +0100

    make department rest ressource crud

--- src/main/java/fr/istic/taa/jaxrs/rest/DepartmentResource.java
+++ src/main/java/fr/istic/taa/jaxrs/rest/DepartmentResource.java
@@ -9,35 +9,61 @@ import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Path("department")
 @Produces({"application/json"})
 public class DepartmentResource {
 
+    DepartmentDao departmentDao = new DepartmentDao();
+
     @GET
     @Path("/{departmentId}")
     public DepartmentDto getDepartmentById(@PathParam("departmentId") Long departmentId)  {
-        // return department
-        DepartmentDao departmentDao = new DepartmentDao();
         Department department =  departmentDao.findOne(departmentId);
-
         return department.toDto();
     }
 
     @GET
     @Path("/")
-    public Department getDepartment(Long departmentId)  {
-        return new Department();
-    }
+    public List<DepartmentDto> getDepartment()  {
+        List<Department> departments =  departmentDao.findAll();
+        return departments.stream().map(Department::toDto).collect(Collectors.toList());
 
+    }
 
     @POST
     @Consumes("application/json")
     public Response addDepartment(
-            @Parameter(name = "Department name", required = true) String name) {
-        // add employee
-        DepartmentDao departmentDao = new DepartmentDao();
-        departmentDao.save(new Department(name));
+            @Parameter(name = "Department name", required = true)
+            DepartmentDto departmentDto) {
+        Department department = new Department(departmentDto.getName());
+        departmentDao.save(department);
+        return Response.status(Response.Status.CREATED).entity("Department created successfully").build();
+    }
 
-        return Response.ok().entity("SUCCESS").build();
+    @PUT
+    @Path("/{departmentId}")
+    public Response updateDepartment(@PathParam("departmentId") Long departmentId, DepartmentDto departmentDto) {
+        Department department = departmentDao.findOne(departmentId);
+        if (department == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Department not found").build();
+        }
+        department.setName(departmentDto.getName());
+        departmentDao.update(department);
+        return Response.ok("Department updated successfully").build();
+    }
+
+    @DELETE
+    @Path("/{departmentId}")
+    public Response deleteDepartment(@PathParam("departmentId") Long departmentId) {
+        Department department = departmentDao.findOne(departmentId);
+        if (department == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Department not found").build();
+        }
+        departmentDao.delete(department);
+        return Response.ok("Department deleted successfully").build();
     }
 }
